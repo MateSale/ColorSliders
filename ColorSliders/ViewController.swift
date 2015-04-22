f985350e65c6bd64d4afa59a4b10f82759976385
MateSale <mate.salekovics@gmail.com>
2015-04-22 15:54:34 +0200

    colorsliders

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    //@IBOutlet weak var redControl: UISwitch!
+    //@IBOutlet weak var greenControl: UISwitch!
+    //@IBOutlet weak var blueControl: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeColorComponent(sender: AnyObject) {
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        // let g: CGFloat = self.greenControl.on ? 1 : 0
+        // let b: CGFloat = self.blueControl.on ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
-
-
 }
 
